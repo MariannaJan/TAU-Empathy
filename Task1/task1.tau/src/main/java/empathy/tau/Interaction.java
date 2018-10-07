b33308433f8cb532661d5b9575dd4eff977f452f
s14292 <s14292@lab3l-4.pjwstk.edu.pl>
2018-10-07 10:35:21 +0200

    created Interaction model

---
+++ Task1/task1.tau/src/main/java/empathy/tau/Interaction.java
@@ -0,0 +1,16 @@
+package empathy.tau;
+
+class Interaction {
+    public String pageNo;
+    public int empathyValue;
+    public int sanityValue;
+    public String interactionDescription;
+    public String optionalJournalEntry;
+    public int empathyTreshold;
+    public int sanityTreshold;
+    public String[] pagesLocked;
+    public String takeItemID;
+    public Boolean OneTimeInteractionFlag;
+    public String RemoveItemId;
+    public boolean PurgeInventoryFlag;
+}
